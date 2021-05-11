6433d1fae7c9daf1b97f47c77203743dd5b21cd5
William Ching <willykamus@gmail.com>
2021-05-11 07:29:14 -0400

    First Commit

---
+++ Jaika/Jaika/JaikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaikaApp.swift
+//  Jaika
+//
+//  Created by William Ching on 2021-05-11.
+//
+
+import SwiftUI
+
+@main
+struct JaikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Jaika/JaikaTests/JaikaTests.swift
@@ -0,0 +1,33 @@
+//
+//  JaikaTests.swift
+//  JaikaTests
+//
+//  Created by William Ching on 2021-05-11.
+//
+
+import XCTest
+@testable import Jaika
+
+class JaikaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
